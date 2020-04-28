fac83f750c7fbafbc743e8a192ff9ad88e2de413
“WalterAbregu” <abreguwalter0709@gmail.com>
2020-04-27 22:29:15 -0500

    creacion de paquete de ejercicios practicos

---
+++ src/ejercicios/Ejercicio1.java
@@ -0,0 +1,16 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		String dato = entrada.next();
+		
+		System.out.println(dato);
+
+	}
+
+}
